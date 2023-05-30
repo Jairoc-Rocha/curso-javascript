67de3fc13e992e41dc714051f2b76019827eeada
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-05-30 20:58:58 -0300

    mais sobre o dom

---
+++ dom/appendChild/script.js
@@ -0,0 +1,6 @@
+let h4 = document.createElement('h4')
+h4.innerText = "Conteúdo do elemento h4 inserido na div principal"
+
+let div = document.querySelector('#container-principal')
+
+div.appendChild(h4)
---
+++ dom/cssJavaScript/script.js
@@ -0,0 +1,9 @@
+let li01 = document.querySelector('#li01')
+let li02 = document.querySelector('#li02')
+let li03 = document.querySelector('#li03')
+
+li01.classList.add('good')
+li02.classList.add('great')
+li03.classList.add('awesome')
+
+li02.classList.remove('great')
