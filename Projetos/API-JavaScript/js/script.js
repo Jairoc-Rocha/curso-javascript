22ddb14fb496a004518a8fda743fc4af7b4bf178
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-06-08 18:14:15 -0300

    Finalizando o projeto API com javascript

--- Projetos/API-JavaScript/js/script.js
+++ Projetos/API-JavaScript/js/script.js
@@ -22,7 +22,7 @@ window.addEventListener('load', () => {
 
     totalPopulationFavorites = document.querySelector('#totalPopulationFavorites')
     
-    numberFormat - Intl.NumberFormat('pt-BR')
+    numberFormat = Intl.NumberFormat('pt-BR')
 
     fetchCountries()
 
@@ -39,6 +39,7 @@ async function fetchCountries() {
             id: numericCode,
             name: translations.pt,
             population,
+            formattedPopulation: formatNumber(population),
             flag,
         }
     })
@@ -58,21 +59,118 @@ function renderCountryList(){
    let countriesHTML = '<div>'
    
    allCountries.forEach(country => {
-    const {name, flag, id, population} = country
+    const {name, flag, id, formattedPopulation} = country
 
     const countryHTML = `
         <div class='country'>
             <div>
+                <a id="${id}" class="waves-effect waves-light btn">+<a/>
             </div>
             <div>
+                <img src="${flag}" alt="${name}">
             </div>
             <div>
+                <ul>
+                    <li>${name}</li>
+                    <li>${formattedPopulation}</li>
+                </ul>
             </div>
         </div>    
     `
-
+    countriesHTML += countryHTML
    })
+
+   countriesHTML += '</div>'
+   tabCountries.innerHTML = countriesHTML
+
 }
-function renderFavorites(){}
-function renderSummary(){}
-function handleCountryButtons(){}
+function renderFavorites(){
+    let favoritesHTML = '<div>'
+
+    favoriteCountries.forEach(country => {
+        const {name, flag, id, formattedPopulation} = country
+
+        const favoriteCountryHTML = `
+            <div class='country'>
+                <div>
+                    <a id="${id}" class="waves-effect waves-light btn red darken-4">-<a/>
+                </div>
+                <div>
+                    <img src="${flag}" alt="${name}">
+                </div>
+                <div>
+                    <ul>
+                        <li>${name}</li>
+                        <li>${formattedPopulation}</li>
+                    </ul>
+                </div>
+            </div>    
+        `  
+        favoritesHTML += favoriteCountryHTML        
+    })
+
+    favoritesHTML += '</div>'
+    tabFavorites.innerHTML = favoritesHTML
+}
+
+function renderSummary(){
+    countCountries.textContent = allCountries.length
+    countFavorites.textContent = favoriteCountries.length
+
+    const totalPopulation = allCountries.reduce((acc, cur) => {
+        return acc + cur.population
+    },0)
+
+    const totalFavorites = favoriteCountries.reduce((acc, cur) => {
+        return acc + cur.population
+    },0)
+
+    totalPopulationList.textContent = formatNumber(totalPopulation)
+    totalPopulationFavorites.textContent = formatNumber(totalFavorites)
+}
+
+function handleCountryButtons(){
+    const countryButtons = Array.from( tabCountries.querySelectorAll('.btn'))
+    const favoriteButtons = Array.from( tabFavorites.querySelectorAll('.btn'))
+
+    countryButtons.forEach(button => {
+        button.addEventListener('click', addToFavorites(button.id))
+    })
+    
+    favoriteButtons.forEach(button => {
+        button.addEventListener('click', removeFromFavorites(button.id))
+    })
+}
+
+function addToFavorites(id) {
+    const countryToAdd = allCountries.find(country => country.id === id)
+
+    favoriteCountries = [...favoriteCountries, countryToAdd]
+
+    favoriteCountries.sort((a, b) => {
+        return a.name.localeCompare(b.name)
+    })
+
+    allCountries = allCountries.filter(country => country.id !== id)
+
+    render()
+    
+}
+
+function removeFromFavorites(id) {
+    const countryToRemove = allCountries.find(country => country.id === id)
+
+    allCountries = [...allCountries, countryToRemove]
+
+    allCountries.sort((a, b) => {
+        return a.name.localeCompare(b.name)
+    })
+
+    favoriteCountries = favoriteCountries.filter(country => country.id !== id)
+
+    render()
+}
+
+function formatNumber(number) {
+    return  numberFormat.format(number)
+}
